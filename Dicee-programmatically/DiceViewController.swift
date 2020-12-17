d7c3db608ae8be2f9cf5e75929f66825c8306dde
SebaKrk <s.sciuba@gmail.com>
2020-12-17 09:50:25 +0100

    set new background color in DiceViewController

--- Dicee-programmatically/DiceViewController.swift
+++ Dicee-programmatically/DiceViewController.swift
@@ -9,12 +9,8 @@ import UIKit
 
 class DiceViewController : UIViewController {
     
-    let backgroudnImage: UIImageView = {
-        let image = UIImageView(image: #imageLiteral(resourceName: "newbackground") )
-        image.contentMode = .scaleAspectFill
-        return image
-    }()
     
+ 
     let diceeView : DiceView = {
         let view = DiceView()
         view.configureViewComponents()
@@ -24,25 +20,19 @@ class DiceViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        view.backgroundColor = UIColor(red: 77/255, green: 86/255, blue: 0/255, alpha: 1)
         configureViewComponents()
-    }
     
+    }
+
     func configureViewComponents() {
-        
-        view.addSubview(backgroudnImage)
-        backgroudnImage.translatesAutoresizingMaskIntoConstraints = false
-        backgroudnImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        backgroudnImage.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        backgroudnImage.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        backgroudnImage.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        
-//        backgroudnImage.addSubview(diceeView)
-//        diceeView.translatesAutoresizingMaskIntoConstraints = false
-//        diceeView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-//        diceeView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-//        diceeView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//        diceeView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    
+        view.addSubview(diceeView)
+        diceeView.translatesAutoresizingMaskIntoConstraints = false
+        diceeView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        diceeView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        diceeView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        diceeView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
     }
     
